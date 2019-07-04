439516be92c80785efc4ef41e9cffea7d5bec077
fjetter <fjetter@users.noreply.github.com>
2019-07-04 13:40:55 +0200

    Try to build pandas from source

--- compile_requirements.sh
+++ compile_requirements.sh
@@ -4,23 +4,31 @@ set -xeo pipefail
 productionIndex=platform
 developmentIndex=platform_dev
 
+touch kartothek_env_reqs.txt
+trap 'rm -f kartothek_env_reqs.txt' EXIT
+
 if [ ! -z ${KARTOTHEK_ARROW_VERSION} ];
 then
-    echo pyarrow==$KARTOTHEK_ARROW_VERSION > kartothek_env_reqs.txt
-    trap 'rm -f kartothek_env_reqs.txt' EXIT
-    pip-compile \
-        --upgrade \
-        --no-index \
-        -o requirements-pinned.txt \
-        kartothek_env_reqs.txt \
-        requirements.txt
-else
-    pip-compile \
-        --upgrade \
-        --no-index \
-        -o requirements-pinned.txt \
-        requirements.txt
+    echo pyarrow==$KARTOTHEK_ARROW_VERSION >> kartothek_env_reqs.txt
 fi
+
+if [ ! -z ${KARTOTHEK_PANDAS_VERSION} ];
+then
+    wget https://github.com/pandas-dev/pandas/releases/download/v0.25.0rc0/pandas-0.25.0rc0.tar.gz
+    tar -xf pandas-0.25.0rc0.tar.gz
+    cd pandas-0.25.0rc0
+    python setup.py bdist_wheel
+
+    echo ${PWD}/dist/pandas-0.25.0rc0-cp36-cp36m-linux_x86_64.whl >> kartothek_env_reqs.txt
+fi
+
+
+pip-compile \
+    --upgrade \
+    --no-index \
+    -o requirements-pinned.txt \
+    kartothek_env_reqs.txt \
+    requirements.txt
 
 pip-compile \
     --upgrade \
